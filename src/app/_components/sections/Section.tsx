d7b7053624caf62e4d3d9e5aa48a5ce46b9bd860
ruancosta07 <ruancosta.ti0805@gmail.com>
2025-03-05 23:44:50 -0300

    feat: componente que identifica qual sessão está ativa

---
+++ src/app/_components/sections/Section.tsx
@@ -0,0 +1,34 @@
+import { useEffect, useRef } from "react";
+
+const Section = ({ id, children, setActiveSection, className }: { id: string; children: React.ReactNode; setActiveSection: (id: string) => void, className?:string }) => {
+    const ref = useRef<HTMLDivElement>(null);
+
+    useEffect(() => {
+        const observer = new IntersectionObserver(
+            ([entry]) => {
+                if (entry.isIntersecting) {
+                    setActiveSection(id);
+                }
+            },
+            { threshold: .3 } // Define quando a seção está visível (50% visível)
+        );
+
+        if (ref.current) {
+            observer.observe(ref.current);
+        }
+
+        return () => {
+            if (ref.current) {
+                observer.unobserve(ref.current);
+            }
+        };
+    }, [id, setActiveSection]);
+
+    return (
+        <section ref={ref} id={id} className={className}>
+            {children}
+        </section>
+    );
+};
+
+export default Section
