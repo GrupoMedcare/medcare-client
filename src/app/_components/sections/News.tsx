9828fc71f9cb7160d3806c2f4c099c58224bc07f
GrupoMedcare <ti@grupomedcare.com.br>
2025-03-10 13:04:50 -0300

    refactor: responsivo

--- src/app/_components/sections/News.tsx
+++ src/app/_components/sections/News.tsx
@@ -1,13 +1,18 @@
+"use client"
 import Image from "next/image";
-import React from "react";
-import { Swiper, SwiperSlide } from "swiper/react";
+import React, { useRef } from "react";
+import { Swiper, SwiperSlide,} from "swiper/react";
 import "swiper/css";
 import "swiper/css/navigation";
 import "swiper/css/pagination";
 import "swiper/css/scrollbar";
 import Link from "next/link";
 import DayJs from "@/utils/dayjs";
+import type { Swiper as SwiperT, SwiperOptions } from "swiper/types";
+import { ChevronLeft, ChevronRight } from "lucide-react";
+import { A11y, Navigation, Pagination } from "swiper/modules";
 const News = () => {
+  const swiperRef = useRef<SwiperT>(null)
   const news = [
     {
       title: "Inauguração do Espaço Reviver na medcare",
@@ -70,13 +75,31 @@ O espaço estará aberto diariamente e contará com entrada gratuita para as div
         "https://images.unsplash.com/photo-1585016031966-dc5e6798f266?q=80&w=1626&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     },
   ];
+
+  const breakpoints:SwiperOptions["breakpoints"] = {
+    0: {
+      slidesPerView: 1
+    },
+    728: {
+      slidesPerView: 2
+    },
+    1024: {
+      slidesPerView: 3
+    }, 
+    1300: {
+      slidesPerView: 4
+    }
+  }
+
   return (
     <section className="mt-[16rem]">
       <div className="container-width">
-        <h1 className="text-[4rem] font-bold text-emerald-700 mb-[1.4rem]">
+        <h1 className="text-[3rem] lg:text-[4rem] font-bold text-emerald-700 mb-[1.4rem]">
           Últimas notícias
         </h1>
-        <Swiper slidesPerView={4} spaceBetween={20} direction="horizontal">
+        <Swiper onSwiper={(swiper)=> {
+          swiperRef.current = swiper
+        }} modules={[Pagination, Navigation, A11y]} breakpoints={breakpoints} spaceBetween={20} direction="horizontal" className="relative">
           {news.map((n, i) => (
             <SwiperSlide key={i} >
               {/* <div> */}
@@ -97,6 +120,14 @@ O espaço estará aberto diariamente e contará com entrada gratuita para as div
               {/* </div> */}
             </SwiperSlide>
           ))}
+          {/* <div className="flex items-center left-0 top-2/4 -translate-y-[120%] z-[2] justify-between"> */}
+          <button className="bg-zinc-950/50 text-zinc-100 p-[.8rem] rounded-full absolute left-0 top-2/4 -translate-y-[120%] z-[3]" onClick={()=> swiperRef.current?.slidePrev()}>
+            <ChevronLeft/>
+          </button>
+          <button className="bg-zinc-950/50 text-zinc-100 p-[.8rem] rounded-full absolute right-0 top-2/4 -translate-y-[120%] z-[3]" onClick={()=> swiperRef.current?.slideNext()}>
+            <ChevronRight/>
+          </button>
+          {/* </div> */}
         </Swiper>
       </div>
     </section>
