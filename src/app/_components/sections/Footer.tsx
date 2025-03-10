4e091f5710411d81e57dd8c4ec1c734bd84053d7
GrupoMedcare <ti@grupomedcare.com.br>
2025-03-10 13:04:43 -0300

    refactor: responsivo

--- src/app/_components/sections/Footer.tsx
+++ src/app/_components/sections/Footer.tsx
@@ -5,7 +5,7 @@ import React from "react";
 const Footer = () => {
   return (
     <footer className="bg-emerald-800 py-[4rem] mt-[16rem]">
-      <div className="container-width grid grid-cols-4 items-start">
+      <div className="container-width grid max-lg:gap-[3rem] lg:grid-cols-4 items-start">
         <Link
           href={"/"}
           className="text-zinc-100 flex items-center gap-[1rem] text-[2.6rem] font-bold"
@@ -28,7 +28,7 @@ const Footer = () => {
           </span>
           <div className="text-[1.4rem] text-zinc-200 mt-[.8rem] grid gap-[.6rem]">
           <a className="" href="tel:3119-7706">Telefone: 3119-7706</a>
-          <a className="" href="https://api.whatsapp.com/send/?phone=5591991242509&text=+&type=phone_number&app_absent=0">Whatsapp: (91) 991242509</a>
+          <a className="" target="_blank" href="https://api.whatsapp.com/send/?phone=5591991242509&text=+&type=phone_number&app_absent=0">Whatsapp: (91) 991242509</a>
           </div>
         </div>
         <div>
@@ -36,10 +36,10 @@ const Footer = () => {
             Redes sociais
           </span>
           <div className="text-[1.4rem] text-zinc-200 mt-[.8rem] flex gap-[1rem]">
-          <a href="" className="border-[1.2px] h-fit w-fit border-zinc-100 text-emerald-100 hover:bg-zinc-100 hover:text-emerald-800 p-[.65rem] rounded-full duration-200">
+          <a href="" className="border-[1.3px] h-fit w-fit border-zinc-100 text-emerald-100 hover:bg-zinc-100 hover:text-emerald-800 p-[.65rem] rounded-full duration-300">
           <Facebook className="size-[2rem]"/>
           </a>
-          <a href="" className="border-[1.2px] h-fit w-fit border-zinc-100 text-emerald-100 hover:bg-zinc-100 hover:text-emerald-800 p-[.65rem] rounded-full duration-200">
+          <a href="" className="border-[1.3px] h-fit w-fit border-zinc-100 text-emerald-100 hover:bg-zinc-100 hover:text-emerald-800 p-[.65rem] rounded-full duration-300">
           <Instagram className="size-[2rem]"/>
           </a>
           </div>
