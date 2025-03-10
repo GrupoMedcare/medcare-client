5a05cdb7a79c2045c661d9d13118817b26c5780a
GrupoMedcare <ti@grupomedcare.com.br>
2025-03-10 13:07:19 -0300

    refactor: responsivo

--- src/app/_components/ui/Header.tsx
+++ src/app/_components/ui/Header.tsx
@@ -1,51 +1,66 @@
 "use client";
-import { Cross } from "lucide-react";
+import { Cross, Menu, X } from "lucide-react";
 import Link from "next/link";
-import React from "react";
-
+import React, { useState } from "react";
+import { usePathname } from "next/navigation";
+import Image from "next/image";
+import useGlobalStore from "@/store/global";
+import {motion} from "framer-motion"
 const Header = ({
   activeSection,
 }: {
   activeSection: string;
   setActiveSection?: (v: string) => void;
 }) => {
+  const [isActiveMenu, setIsActiveMenu] = useState(false)
+  const {isMobile} = useGlobalStore()
+
   const scrollToElement = (value: string) => {
     const section = document.getElementById(value) as HTMLElement;
-      section.scrollIntoView({
-        behavior: "smooth",
-        block: "center",
-      });
+    section.scrollIntoView({
+      behavior: "smooth",
+      block: "center",
+    });
   };
+  const pathname = usePathname();
 
   return (
     <header className="bg-emerald-800 sticky top-0 z-[3] shadow-xl">
-      <div className="container-width py-[3rem] flex items-center justify-between">
+      <div className="container-width py-[1.6rem] lg:py-[3rem] flex items-center justify-between">
         <Link
           href={"/"}
-          className="text-zinc-50 text-[2.6rem] font-semibold flex items-center gap-[.6rem]"
+          className="text-zinc-50 text-[2.6rem] font-semibold flex items-center gap-[1rem]"
         >
-          <Cross />
+          <Image src={"/logo.png"} alt="logo" width={28} height={28}/>
           Medcare
         </Link>
-        <nav className="text-zinc-100 flex justify-center items-center w-full">
-          <ul className="flex gap-[1rem] text-[1.7rem] font-medium mx-auto">
-            <li className={"p-[1rem]"}>
+        {isMobile && <button onClick={()=> setIsActiveMenu(v=> !v)} className="text-zinc-100 p-[1rem] lg:hidden">
+          {isActiveMenu ? <X className="size-[3rem]"/>: <Menu className="size-[3rem]"/>}
+        </button>}
+        {(isActiveMenu || !isMobile) &&  <motion.nav initial={!isActiveMenu && isMobile ?  {right: "-100%", opacity:0}: {}} animate={isActiveMenu && isMobile ?{right: "0%", opacity:1}: {}} className="text-zinc-100 flex justify-center lg:items-center w-full max-lg:absolute max-lg:flex-col max-lg:bg-emerald-800 max-lg:h-dvh max-lg:top-0 max-lg:w-[70%] z-[2] max-lg:p-[3rem] lg:relative max-lg:right-[-100%] ">
+          {isActiveMenu && isMobile && <button onClick={()=> setIsActiveMenu(false)} className="p-[.8rem] text-zinc-100 absolute left-4 top-4 border border-zinc-100 rounded-2">
+            <X className="size-[3rem] "/>
+          </button>}
+          <ul className="flex gap-[1rem] text-[1.7rem] font-medium lg:mx-auto max-lg:flex-col">
+            <li className={"py-[1rem] lg:p-[1rem]"}>
               <Link
-                onClick={(e) =>{
-                    //  scrollToElement("#")
-                    //  e.preventDefault()
+                onClick={(e) => {
+                  if (pathname === "/") {
+                    scrollToElement("inicio");
+                    e.preventDefault();
+                  }
                 }}
-                data-active={activeSection === "main"}
-                href={"/#"}
+                data-active={activeSection === "inicio"}
+                href={pathname === "/" ? "#inicio" : "/#"}
               >
                 Início
               </Link>
             </li>
-            <li className={"p-[1rem]"}>
+            <li className={"py-[1rem] lg:p-[1rem]"}>
               <Link
-                onClick={(e) =>{
-                     scrollToElement("servicos")
-                     e.preventDefault()
+                onClick={(e) => {
+                  scrollToElement("servicos");
+                  e.preventDefault();
                 }}
                 data-active={activeSection === "servicos"}
                 href={"#servicos"}
@@ -53,11 +68,11 @@ const Header = ({
                 Serviços
               </Link>
             </li>
-            <li className={"p-[1rem]"}>
+            <li className={"py-[1rem] lg:p-[1rem]"}>
               <Link
-                onClick={(e) =>{
-                     scrollToElement("sobre")
-                     e.preventDefault()
+                onClick={(e) => {
+                  scrollToElement("sobre");
+                  e.preventDefault();
                 }}
                 data-active={activeSection === "sobre"}
                 href={"/"}
@@ -65,11 +80,11 @@ const Header = ({
                 Sobre nós
               </Link>
             </li>
-            <li className={"p-[1rem]"}>
+            <li className={"py-[1rem] lg:p-[1rem]"}>
               <Link
-                onClick={(e) =>{
-                     scrollToElement("especialidades")
-                     e.preventDefault()
+                onClick={(e) => {
+                  scrollToElement("especialidades");
+                  e.preventDefault();
                 }}
                 data-active={activeSection === "especialidades"}
                 href={"/"}
@@ -77,11 +92,11 @@ const Header = ({
                 Especialidades
               </Link>
             </li>
-            <li className={"p-[1rem]"}>
+            <li className={"py-[1rem] lg:p-[1rem]"}>
               <Link
-                onClick={(e) =>{
-                     scrollToElement("contato")
-                     e.preventDefault()
+                onClick={(e) => {
+                  scrollToElement("contato");
+                  e.preventDefault();
                 }}
                 data-active={activeSection === "contato"}
                 href={"/"}
@@ -91,12 +106,16 @@ const Header = ({
             </li>
           </ul>
           <Link
-            className="bg-zinc-100 text-emerald-800 text-[1.6rem] p-[1rem] px-[1.4rem] rounded-[.4rem] font-semibold"
+            className="bg-zinc-100 text-emerald-800 text-[1.6rem] p-[1rem] px-[1.4rem] rounded-1 font-semibold max-lg:mt-[.8rem]"
             href={"/"}
+            onClick={(e)=> {
+              e.preventDefault()
+              window.open("https://api.whatsapp.com/send/?phone=5591991242509&text=+&type=phone_number&app_absent=0")
+            }}
           >
             Agendar consulta
           </Link>
-        </nav>
+        </motion.nav>}
       </div>
     </header>
   );
--- src/app/_components/ui/ServiceCard.tsx
+++ src/app/_components/ui/ServiceCard.tsx
@@ -1,14 +1,21 @@
 import { Service } from '@/types/general'
-import React from 'react'
+import React, { useRef, useState } from 'react'
 import {motion} from "framer-motion"
 const ServiceCard = ({icon, text, title, delay}:Service & {delay:number}) => {
+  const [isAnimated, setIsAnimated] = useState(false)
+  const timeOut = useRef<NodeJS.Timeout>(null)
   return (
-    <motion.div initial={{opacity:0, scale:.9,}} whileInView={{opacity:1, scale:1,}}  transition={{delay: delay * .1}} className='text-center p-[2rem] border border-zinc-300 rounded-[1rem] hover:border-emerald-800 duration-200'>
+    <motion.div onViewportEnter={()=> {
+      timeOut.current = setTimeout(()=> {
+        setIsAnimated(true)
+      }, 300)
+      // clearTimeout(timeOut.current as NodeJS.Timeout)
+    }} initial={!isAnimated ? {opacity:0, scale:.9,}: {opacity:1, scale:1}} whileInView={!isAnimated ? {opacity:1, scale:1,}: {}}  transition={{delay: delay * .1}} className='text-center p-[2rem] border border-zinc-300 rounded-[1rem] hover:border-emerald-700 hover:shadow-green-1 duration-200'>
       <div className='flex justify-center mb-[1rem] text-emerald-800 bg-emerald-200 p-[1rem] rounded-full w-fit mx-auto'>
       {icon}
       </div>
-      <span className='xl:text-[2rem] 2xl:text-[2.4rem] text-emerald-800 font-bold mb-[.8rem] block'>{title}</span>
-      <p className='xl:text-[1.4rem] 2xl:text-[1.6rem] leading-[1.3]'>{text}</p>
+      <span className='text-[1.8rem] xl:text-[2rem] 2xl:text-[2.4rem] text-emerald-800 font-bold mb-[.8rem] block'>{title}</span>
+      <p className='text-[1.4rem] xl:text-[1.4rem] 2xl:text-[1.6rem] leading-[1.3]'>{text}</p>
     </motion.div>
   )
 }
