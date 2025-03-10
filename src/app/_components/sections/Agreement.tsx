4449a4a1ed24f5cde637d97149c9132d7823ac88
GrupoMedcare <ti@grupomedcare.com.br>
2025-03-10 13:03:29 -0300

    refactor: responsivo e adição dos conv~enios

--- src/app/_components/sections/Agreement.tsx
+++ src/app/_components/sections/Agreement.tsx
@@ -1,16 +1,26 @@
+"use client"
 import Image from 'next/image'
-import React from 'react'
-
+import React, { useRef, useState } from 'react'
+import {motion} from "framer-motion"
 const Agreement = () => {
+    const [isAnimated, setIsAnimated] = useState(false)
+    const timeOut = useRef<NodeJS.Timeout>(null)
+    const convenios = ["iasep.webp", "unimed.webp", "hapvida.webp", "geap.webp", "sintrebel.webp", "pasa.webp", "trt-8.webp", "tre-pa.webp", "sepaco.webp", "iasb.webp", "aspeb.webp", "sul-america.webp", "e-vida.webp", "cassi.webp", "bradesco.webp"];
+    
     return (
         <section className='mt-[16rem]'>
             <div className="container-width">
-                {/* <div className="col-span-full mb-[2rem]"> */}
                     <span className='bg-emerald-200 text-emerald-800 block w-fit mx-auto text-[1.4rem] p-[1rem] font-semibold rounded-[1rem] mb-[1.2rem]'>Convênios</span>
-                    <h1 className='text-[4rem] font-bold text-emerald-800 text-center mb-[.8rem]'>Aceitamos os principais convênios </h1>
-                    {/* <p className='text-[1.6rem] leading-[1.3] text-zinc-700 text-center max-w-[60ch] mx-auto'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Facilis repudiandae voluptates repellat cumque laboriosam at sed, reprehenderit dolorum incidunt iste officiis.</p> */}
-                    <div className='grid grid-cols-5 gap-[1rem] mt-[2rem]'>
-                    {Array.from({length:15}).map((a,i)=> <Image key={i} src="https://images.unsplash.com/photo-1582750433449-648ed127bb54?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt='asd' width={500} height={500} className='size-[16rem] w-full object-cover'/>)}
+                    <h1 className='text-[3rem] lg:text-[4rem] font-bold text-emerald-800 text-center mb-[.8rem]'>Aceitamos os principais convênios </h1>
+                    <div className='grid grid-cols-3 lg:grid-cols-5 gap-[1rem] mt-[2rem]'>
+                    {convenios.map((a,i)=> <motion.div onViewportEnter={()=> {
+                        // clearTimeout(timeOut.current as NodeJS.Timeout)
+                        timeOut.current = setTimeout(()=> {
+                            setIsAnimated(true)
+                        }, 300)
+                    }} initial={!isAnimated ? {opacity:0, scale:.9}: {opacity:1, scale:1}} whileInView={!isAnimated ? {opacity:0, scale:.9}: {opacity:1, scale:1}} transition={{delay: i* .15}} key={i}>
+                        <Image  src={`/${a}`} alt={`Imagem do plano de saúde ${a.split(".")[0]}`} width={500} height={500} className='size-[12rem] w-full object-contain'/>
+                    </motion.div>)}
                     </div>
                 {/* </div> */}
             </div>
