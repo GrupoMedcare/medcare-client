a83d2087be23bd8ad69a9867b3fd41d4465957f0
GrupoMedcare <ti@grupomedcare.com.br>
2025-03-10 13:01:24 -0300

    feat: classes customizadas

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,7 +3,16 @@ import { Config } from "tailwindcss";
 module.exports = {
   content: ["./src/**/*.tsx"],
   theme: {
-    extend: {},
+    extend: {
+      borderRadius: {
+        1: ".6rem",
+        2: ".8rem",
+        3: "1rem"
+      },
+      boxShadow: {
+        "green-1": "0 0 0px 1px #065f46, 0 0 1px 3px #065f4681"
+      }
+    },
   },
   plugins: [],
 } as Config
